8735b9efc626b18d2d594d89af62f91be015b371
hyxx-su <lulanglangl@gmail.com>
2025-07-09 09:33:23 +0900

    feat : tk-10

--- 2025-07-09-TK/tk-10.py
+++ 2025-07-09-TK/tk-10.py
@@ -7,6 +7,20 @@ root.geometry("300x200") # 창 크기 지정
 top_btn = Button(root, text = "Top")
 top_btn.pack(side = "top", fill = "x") # 위쪽에 배치하고 가로로 확장
 
+# 아래쪽에 가로로 확장한 버튼
+bottom_btn = Button(root, text = "Bottom")
+bottom_btn.pack(side = "bottom", fill = "x") # 위쪽에 배치하고 가로로 확장
 
+# 왼쪽에 가로로 확장한 버튼
+left_btn = Button(root, text = "Left")
+left_btn.pack(side = "left", fill = "x") # 위쪽에 배치하고 가로로 확장
+
+# 오른쪽에 가로로 확장한 버튼
+right_btn = Button(root, text = "Right")
+right_btn.pack(side = "right", fill = "x") # 위쪽에 배치하고 가로로 확장
+
+# 중앙에 가로로 확장한 버튼
+center_label = Label(root, text = "Center Area", bg = "lightgray") # 중앙 영역을 나타내는 레이블
+center_label.pack(expand = True, fill = "both") # 위쪽에 배치
 
 root.mainloop() # root 닫을 때까지 이벤트 루프에서 대기1
